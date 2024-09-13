d65c905926a6aaec7e3dcc5990b436c6df70162a
PH6 <101449457+WlphM@users.noreply.github.com>
2024-09-13 21:29:08 +0800

    Update ioctl_cfg80211.c

--- drivers/net/wireless/realtek/rtl8812au/os_dep/linux/ioctl_cfg80211.c
+++ drivers/net/wireless/realtek/rtl8812au/os_dep/linux/ioctl_cfg80211.c
@@ -6760,7 +6760,7 @@ static int cfg80211_rtw_del_virtual_intf(struct wiphy *wiphy,
 		pwdev_priv = adapter_wdev_data(adapter);
 
 		if (ndev == pwdev_priv->pmon_ndev) {
-			cfg80211_uncfg80211_register_netdevice(ndev);
+			cfg80211_unregister_netdevice(ndev);
 			pwdev_priv->pmon_ndev = NULL;
 			pwdev_priv->ifname_mon[0] = '\0';
 			RTW_INFO(FUNC_NDEV_FMT" remove monitor ndev\n", FUNC_NDEV_ARG(ndev));
